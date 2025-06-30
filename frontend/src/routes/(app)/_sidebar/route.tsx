d5b8e222a03466e848cbeec4b5ccc699297ec50c
anthonycifuentes <anthonycifu@gmail.com>
2025-06-29 21:26:43 -0600

    initial commit

--- frontend/src/routes/(app)/_sidebar/route.tsx
+++ frontend/src/routes/(app)/_sidebar/route.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/core/components/layout/layout"
 import { createFileRoute } from "@tanstack/react-router"
 
+// @ts-ignore
 export const Route = createFileRoute("/(app)/_sidebar")({
   component: RouteComponent,
 })
