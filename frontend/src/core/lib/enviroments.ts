9c492082081436cb4ca3144fe010faf6305fbf33
anthonycifuentes <anthonycifu@gmail.com>
2025-06-29 22:59:07 -0600

    fix: update API URL to remove versioning in environment configuration

--- frontend/src/core/lib/enviroments.ts
+++ frontend/src/core/lib/enviroments.ts
@@ -1,5 +1,5 @@
 export const ENVIROMENT = {
   VITE_API_URL:
     import.meta.env.VITE_API_URL ||
-    "https://ai-career-explorer-backend.vercel.app/api/v1",
+    "https://ai-career-explorer-backend.vercel.app/api",
 }
